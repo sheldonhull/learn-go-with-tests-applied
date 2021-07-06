d61c2fffef097799c476db26165724d78557be07
Sheldon Hull <sheldonhull@users.noreply.github.com>
2021-07-06 18:16:17 -0500

    feat: include t.Helper() function
    https://quii.gitbook.io/learn-go-with-tests/go-fundamentals/hello-world#hello-world-again

--- src/001-hello-world/hello.go
+++ src/001-hello-world/hello.go
@@ -5,10 +5,13 @@ import "fmt"
 const englishHelloPrefix = "Hello, "
 
 func main() {
-	fmt.Println(Hello("Hello"))
+	fmt.Println(Hello(""))
 }
 
 // Hello returns a string
 func Hello(name string) string {
+	if name== "" {
+		return englishHelloPrefix + "World"
+	}
 	return englishHelloPrefix + name
 }
