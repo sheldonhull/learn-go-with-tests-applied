f58f77676f04f5103fae95e15d9046a604ad05ca
Sheldon Hull <sheldonhull@users.noreply.github.com>
2021-07-06 18:05:12 -0500

    feat: adjust hello world to use constants

--- src/001-hello-world/hello.go
+++ src/001-hello-world/hello.go
@@ -2,11 +2,13 @@ package main
 
 import "fmt"
 
+const englishHelloPrefix = "Hello, "
+
 func main() {
 	fmt.Println(Hello("Hello"))
 }
 
 // Hello returns a string
 func Hello(name string) string {
-	return fmt.Sprintf("Hello, %s", name)
+	return englishHelloPrefix + name
 }
