bea49fc2f27e6bbc1151cd04344525377044caeb
Sheldon Hull <sheldonhull@users.noreply.github.com>
2021-07-12 19:54:16 -0500

    refactor: ✅ modify array to use slices for flexible input sizes

--- src/004-arrays-and-slices/slicer.go
+++ src/004-arrays-and-slices/slicer.go
@@ -1,9 +1,10 @@
 package slicer
 
-func Sum(numbers [5]int) int {
+func Sum(numbers []int) int {
 	sum := 0
 	for _, n := range numbers {
 		sum += n
 	}
+
 	return sum
 }
--- src/004-arrays-and-slices/slicer_test.go
+++ src/004-arrays-and-slices/slicer_test.go
@@ -1,17 +1,16 @@
 package slicer_test
 
 import (
-	"testing"
-
 	"slicer"
+	"testing"
 
 	"github.com/matryer/is"
 )
 
 func TestSum(t *testing.T) {
 	is := is.New(t)
-	got := slicer.Sum([5]int{1,1,1,1,1})
+	got := slicer.Sum([]int{1, 1, 1, 1, 1})
 	want := 5
 
-	is.Equal(got,want) // sum should return 3
+	is.Equal(got, want) // sum should return 3
 }
